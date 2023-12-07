ab5baac139b246dd7b747cfc290d91fc4259e69f
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2023-12-07 19:20:21 +0530

    Update NativeBanner.tsx

--- src/components/Ads/NativeBanner.tsx
+++ src/components/Ads/NativeBanner.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 
 export default function NativeBanner(): JSX.Element {
-    const containerId = 'container-1f9fba3a4c42eeba308fbf4563eb0668';
+    const containerId = 'container-8045677dad9f66052e176c8217b3a3f7';
     const banner = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
@@ -13,7 +13,7 @@ export default function NativeBanner(): JSX.Element {
                 const script = document.createElement('script');
                 script.async = true;
                 script.dataset.cfasync = 'false';
-                script.src = '//pl18066919.toprevenuegate.com/1f9fba3a4c42eeba308fbf4563eb0668/invoke.js';
+                script.src = '//pl18066919.toprevenuegate.com/8045677dad9f66052e176c8217b3a3f7/invoke.js';
 
                 banner.current.appendChild(script);
 
